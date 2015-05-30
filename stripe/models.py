9410a6c8038bafaf0076a0548fec42d89ed71c87
Tony Narlock <tony@git-pull.com>
2015-05-30 04:35:24 -0500

    Stubs for the stripe models.

--- stripe/models.py
+++ stripe/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django_extensions.db.fields import json
 
 
-class Customer(models.Model):
+class Charge(models.Model):
     livemode = models.BooleanField(default=True)
     created = models.DateTimeField()
     account_balance = models.IntegerField()
@@ -18,3 +18,55 @@ class Customer(models.Model):
     metadata = json.JSONField()
     sources = json.JSONField()
     # subscriptions = models.ManyToManyField(Subscription)
+
+
+class Refund(models.Model):
+    pass
+
+class Customer(models.Model):
+    pass
+
+class Card(models.Model):
+    pass
+
+class Discount(models.Model):
+    pass
+
+class Invoice(models.Model):
+    pass
+
+class InvoiceItem(models.Model):
+    pass
+
+class Dispute(models.Model):
+    pass
+
+class Transfer(models.Model):
+    pass
+
+class TransferReversal(models.Model):
+    pass
+
+class Recipient(models.Model):
+    pass
+
+class ApplicationFee(models.Model):
+    pass
+
+class ApplicationFeeRefund(models.Model):
+    pass
+
+class Account(models.Model):
+    pass
+
+class Balance(models.Model):
+    pass
+
+class Event(models.Model):
+    pass
+
+class Token(models.Model):
+    pass
+
+class BitCoinReceiver(models.Model):
+    pass
