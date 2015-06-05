e1c90d6e041ffec526f1d10eb7b4e1a8561e876d
Tony Narlock <tony@git-pull.com>
2015-06-05 00:48:29 -0500

    DisputeEvidnece the rest of the fields.

--- models.py
+++ models.py
@@ -1043,6 +1043,149 @@ class DisputeEvidence(models.Model):
             "shown to the customer."
         )
     )
+    cancellation_policy_disclosure = models.TextField(
+        help_text=_(
+            "An explanation of how and when the customer was shown your "
+            "refund policy prior to purchase."
+        )
+    )
+    cancellation_rebuttal = models.TextField(
+        help_text=_(
+            "A justification for why the customer’s subscription was not "
+            "canceled."
+        )
+    )
+    customer_communication = models.ForeignKey(
+        "FileUpload",
+        help_text=_(
+            "(ID of a file upload) Any communication with the customer that "
+            "you feel is relevant to your case (for example emails proving "
+            "that they received the product or service, or demonstrating their "
+            "use of or satisfaction with the product or service)"
+        )
+    )
+    customer_email_address = models.EmailField()
+    customer_name = models.CharField(max_length=255)
+    customer_purchase_ip = models.CharField(max_length=255)
+    customer_signature = models.ForeignKey(
+        "FileUpload",
+        help_text=_(
+            "(ID of a file upload) A relevant document or contract showing the "
+            "customer’s signature."
+        )
+    )
+    duplicate_change_documentation = models.ForeignKey(
+        "FileUpload",
+        help_text=(
+            "ID of a file upload) Documentation for the prior charge that can "
+            "uniquely identify the charge, such as a receipt, shipping label, "
+            "work order, etc. This document should be paired with a similar "
+            "document from the disputed payment that proves the two payments "
+            "are separate."
+        )
+    )
+    duplicate_charge_explanation = models.TextField(
+        help_text=_(
+            "An explanation of the difference between the disputed charge and "
+            "the prior charge that appears to be a duplicate."
+        )
+    )
+    duplicate_charge_id = models.ForeignKey(
+        "Charge",
+        help_text=_(
+            "The Stripe ID for the prior charge which appears to be a "
+            "duplicate of the disputed charge."
+        )
+    )
+    product_description = models.TextField(
+        help_text=_(
+            "A description of the product or service which was sold."
+        )
+    )
+    receipt = models.ForeignKey(
+        "FileUpload",
+        help_text=_(
+            "(ID of a file upload) Any receipt or message sent to the customer "
+            "notifying them of the charge."
+        )
+    )
+    refund_policy = models.ForeignKey(
+        "FileUpload",
+        help_text=_(
+            "(ID of a file upload) Your refund policy, as shown to the "
+            "customer."
+        )
+    )
+    refund_policy_disclosure = models.TextField(
+        help_text=_(
+            "Documentation demonstrating that the customer was shown your "
+            "refund policy prior to purchase."
+        )
+    )
+    refund_refusal_explanation = models.TextField(
+        help_text=_(
+            "A justification for why the customer is not entitled to a refund."
+        )
+    )
+    service_date = models.DateTimeField(
+        help_text=_(
+            "The date on which the customer received or began receiving the "
+            "purchased service, in a clear human-readable format."
+        )
+    )
+    service_documentation = models.ForeignKey(
+        "FileUpload",
+        help_text=_(
+            "(ID of a file upload) Documentation showing proof that a service "
+            "was provided to the customer. This could include a copy of a "
+            "signed contract, work order, or other form of written agreement."
+        )
+    )
+    shipping_address = models.TextField(
+        help_text=_(
+            "The address to which a physical product was shipped. You should "
+            "try to include as much complete address information as possible."
+        )
+    )
+    shipping_carrier = models.TextField(
+        help_text=_(
+            "The delivery service that shipped a physical product, such as "
+            "Fedex, UPS, USPS, etc. If multiple carriers were used for this "
+            "purchase, please separate them with commas."
+        )
+    )
+    shipping_date = models.DateTimeField(
+        help_text=_(
+            "The date on which a physical product began its route to the "
+            "shipping address, in a clear human-readable format."
+        )
+    )
+    shipping_documentation = models.ForeignKey(
+        "FileUpload",
+        help_text=_(
+            "(ID of a file upload) Documentation showing proof that a product "
+            "was shipped to the customer at the same address the customer "
+            "provided to you. This could include a copy of the shipment "
+            "receipt, shipping label, etc, and should show the full shipping "
+            "address of the customer, if possible."
+        )
+    )
+    shipping_tracking_number = models.TextField(
+        help_text=_(
+            "The tracking number for a physical product, obtained from the "
+            "delivery service. If multiple tracking numbers were generated for "
+            "this purchase, please separate them with commas."
+        )
+    )
+    uncategorized_file = models.ForeignKey(
+        "FileUpload",
+        help_text=_(
+            "(ID of a file upload) Any additional evidence or statements."
+        )
+    )
+    uncategorized_text = models.TextField(
+        help_text=_("Any additional evidence or statements.")
+    )
 
 class Transfer(models.Model):
     pass
