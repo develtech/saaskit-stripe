6a6b8972ea54e1a94cfe08c6e4fbcb3a3ed85f6e
Tony Narlock <tony@git-pull.com>
2015-06-10 12:07:55 -0500

    Balance model.

--- models.py
+++ models.py
@@ -1863,7 +1863,19 @@ class Balance(models.Model):
     list of transactions that have ever contributed to the balance (charges,
     refunds, transfers, and so on).
     """
-    pass
+    livemode = models.BooleanField()
+    available = json.JSONField(
+        help_text=(
+            "Funds that are available to be paid out automatically by Stripe "
+            "or explicitly via the transfers API."
+        )
+    )
+    pending = json.JSONField(
+        help_text=_(
+            "Funds that are not available in the balance yet, due to the 7-day "
+            "rolling pay cycle."
+        )
+    )
 
 class BalanceTransaction(models.Model):
     pass
