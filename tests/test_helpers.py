5af2dc1122d0ea099b60c0a4c3edf17a2b102d23
Tony Narlock <tony@git-pull.com>
2015-06-19 02:55:51 -0500

    Add test for mocking stripe response.

---
+++ tests/test_helpers.py
@@ -0,0 +1,39 @@
+"""Test the helpers for the unittests."""
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import json
+
+from django.test import TestCase
+
+import requests
+import responses
+import stripe
+
+stripe.api_key = "sk_test_BQokikJOvBiI2HlWgH4olfQ2"
+
+from .helpers import open_test_file, json_file_to_dict
+
+
+class TestResponsesStripeSanity(TestCase):
+    """Tests to assure the Stripe library is using the Requests
+    library as an HTTP Client and responses is mocking the
+    responses as intended."""
+
+    @responses.activate
+    def test_my_api(self):
+        body = open_test_file("customer.json")
+        body_json = json_file_to_dict(body)
+
+        customer_id = "cus_6Ozta4Bn1hmWEH"
+        customer_url = "https://api.stripe.com/v1/customers/%s" % customer_id
+        responses.add(responses.GET, customer_url,
+                    body=json.dumps(body_json), status=200,
+                    content_type='application/json')
+
+        customer = stripe.Customer.retrieve(customer_id)
+
+        self.assertEquals(customer.id, customer_id)
+        self.assertEquals(len(responses.calls), 1)
+        self.assertEquals(responses.calls[0].request.url, customer_url)
+        self.assertEquals(responses.calls[0].response.text, json.dumps(body_json))
