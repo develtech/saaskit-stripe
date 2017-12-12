ae68a3a075d0801c3ed252aa5599d415d0880648
Tony Narlock <tony@git-pull.com>
2017-12-12 11:56:51 -0600

    initial import of old stripe module from devel.tech

---
+++ tests/test_helpers.py
@@ -0,0 +1,37 @@
+"""Test the helpers for the unittests."""
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.test import TestCase
+
+import responses
+import stripe
+
+from .helpers import open_test_file
+
+stripe.api_key = "sk_test_BQokikJOvBiI2HlWgH4olfQ2"
+
+
+class TestResponsesStripeSanity(TestCase):
+    """Tests to assure the Stripe library is using the Requests
+    library as an HTTP Client and responses is mocking the
+    responses as intended."""
+
+    @responses.activate
+    def test_my_api(self):
+        body_json = open_test_file("customer.json").read()
+
+        customer_id = "cus_6Ozta4Bn1hmWEH"
+        customer_url = "https://api.stripe.com/v1/customers/%s" % customer_id
+        responses.add(
+            responses.GET, customer_url,
+            body=body_json, status=200,
+            content_type='application/json'
+        )
+
+        customer = stripe.Customer.retrieve(customer_id)
+
+        self.assertEquals(customer.id, customer_id)
+        self.assertEquals(len(responses.calls), 1)
+        self.assertEquals(responses.calls[0].request.url, customer_url)
+        self.assertEquals(responses.calls[0].response.text, body_json)
