a9c6841fa5ade9be26244aaab026c170e432e464
Tony Narlock <tony@git-pull.com>
2017-12-13 09:20:08 -0600

    yapf some stripe models, part 1

--- models/account.py
+++ models/account.py
@@ -20,143 +20,135 @@ class Account(models.Model):
     """
 
     charges_enabled = models.BooleanField(
-        help_text=_(
-            'Whether or not the account can create live charges'
-        )
-    )
+        help_text=_('Whether or not the account can create live charges'))
     country = models.CharField(  # todo: add CHOICES
         max_length=255,
         help_text=_('The country of the account')
     )
     currencies_supports = json.JSONField(
         help_text=_(
-            'The currencies this account can submit when creating charges'
-        )
+            'The currencies this account can submit when creating charges',
+        ),
     )
     default_currency = models.CharField(
-        max_length=255,
-        help_text=_(
-            'The currency this account has chosen to use as the default'
-        ),
-        choices=CURRENCY_CHOICES
-    )
+        max_length=255, help_text=_(
+            'The currency this account has chosen to use as the default'),
+        choices=CURRENCY_CHOICES)
     details_submitted = models.BooleanField(
         help_text=_(
             'Whether or not account details have been submitted yet. '
-            'Standalone accounts cannot receive transfers before this is true.'
-        )
+            'Standalone accounts cannot receive transfers before this is true.',
+        ),
     )
     transfers_enabled = models.BooleanField(
         help_text=_(
             'Whether or not Stripe will send automatic transfers for this '
             'account. This is only false when Stripe is waiting for '
-            'additional information from the account holder.'
-        )
+            'additional information from the account holder.',
+        ),
     )
     display_name = models.CharField(
         max_length=255,
         help_text=_(
             'The display name for this account. This is used on the Stripe '
-            'dashboard to help you differentiate between accounts.'
-        )
+            'dashboard to help you differentiate between accounts.',
+        ),
     )
-    email = models.EmailField(
-        help_text=_('The primary user’s email address')
-    )
+    email = models.EmailField(help_text=_('The primary user’s email address'))
     statement_descriptor = models.TextField(
-        help_text=_('The text that will appear on credit card statements')
+        help_text=_(
+            'The text that will appear on credit card statements',
+        ),
     )
     timezone = models.CharField(
         max_length=255,
         help_text=_(
             'The timezone used in the Stripe dashboard for this account. A '
             'list of possible timezone values is maintained at the IANA '
-            'Timezone Database.'
-        )
+            'Timezone Database.',
+        ),
     )
     business_name = models.CharField(
         max_length=255,
         help_text=_(
-            'The publicly visible name of the business'
-        )
+            'The publicly visible name of the business',
+        ),
     )
     business_url = models.URLField(
-        help_text=_(
-            'The publicly visible website of the business'
-        )
+        help_text=_('The publicly visible website of the business'),
     )
     metadata = json.JSONField(
         help_text=_(
             'A set of key/value pairs that you can attach to a charge object. '
             'it can be useful for storing additional information about the '
-            'charge in a structured format.'
-        )
+            'charge in a structured format.',
+        ),
     )
     support_phone = models.CharField(
         max_length=255,
         help_text=_(
-            'The publicly visible support phone number for the business'
-        )
+            'The publicly visible support phone number for the business',
+        ),
     )
     managed = models.BooleanField(
         help_text=_(
             'Whether or not the account is managed by your platform. Returns '
-            'null if the account was not created by a platform.'
-        )
+            'null if the account was not created by a platform.',
+        ),
     )
     bank_accounts = json.JSONField(
         help_text=_(
             '(Managed Accounts Only) '
-            'Bank accounts currently attached to this account.'
-        )
+            'Bank accounts currently attached to this account.',
+        ),
     )
     debit_negative_balances = models.BooleanField(
         help_text=_(
             '(Managed Accounts Only) '
             'Whether or not Stripe will attempt to reclaim negative account '
-            'balances from this account’s bank account.'
-        )
+            'balances from this account’s bank account.',
+        ),
     )
     decline_charge_on = json.JSONField(
         help_text=_(
             '(Managed Accounts Only) '
             'Account-level settings to automatically decline certain types of '
-            'charges regardless of the bank’s decision.'
-        )
+            'charges regardless of the bank’s decision.',
+        ),
     )
     legal_entity = json.JSONField(
         help_text=_(
             '(Managed Accounts Only) '
             'Information regarding the owner of this account, including '
-            'verification status.'
-        )
+            'verification status.',
+        ),
     )
     product_description = models.TextField(
         help_text=_(
             '(Managed Accounts Only) '
             'An internal-only description of the product or service provided. '
             'This is used by Stripe in the event the account gets flagged for '
-            'potential fraud.'
-        )
+            'potential fraud.',
+        ),
     )
     tos_acceptance = json.JSONField(
         help_text=_(
             '(Managed Accounts Only) '
             'Who accepted the Stripe terms of service, and when they accepted '
-            'it.'
-        )
+            'it.',
+        ),
     )
     transfer_schedule = json.JSONField(
         help_text=_(
             '(Managed Accounts Only) '
             'When payments collected will be automatically paid out to the '
-            'account holder’s bank account'
-        )
+            'account holder’s bank account',
+        ),
     )
     verfication = json.JSONField(
         help_text=_(
             '(Managed Accounts Only) '
             'That state of the account’s information requests, including what '
-            'information is needed and by when it must be provided.'
-        )
+            'information is needed and by when it must be provided.',
+        ),
     )
--- models/balance_transaction.py
+++ models/balance_transaction.py
@@ -9,59 +9,51 @@ from .charge import CURRENCY_CHOICES
 
 class BalanceTransaction(models.Model):
     amount = models.IntegerField(
-        help_text=_(
-            'Gross amount of the transaction, in cents'
-        )
+        help_text=_('Gross amount of the transaction, in cents'),
     )
     available_on = models.DateTimeField(
         help_text=_(
             'The date the transaction’s net funds will become available in '
-            'the Stripe balance.'
-        )
+            'the Stripe balance.',
+        ),
     )
     created = models.DateTimeField()
-    currency = models.CharField(
-        max_length=255,
-        choices=CURRENCY_CHOICES
-    )
+    currency = models.CharField(max_length=255, choices=CURRENCY_CHOICES)
     fee = models.IntegerField(
         help_text=_(
-            'Fees (in cents) paid for this transaction'
-        )
+            'Fees (in cents) paid for this transaction',
+        ),
     )
     fee_details = json.JSONField(
         help_text=_(
-            'Detailed breakdown of fees (in cents) paid for this transaction'
-        )
+            'Detailed breakdown of fees (in cents) paid for this transaction',
+        ),
     )
     net = models.IntegerField(
-        help_text=_(
-            'Net amount of the transaction, in cents.'
-        )
-    )
+        help_text=_('Net amount of the transaction, in cents.'))
     status = models.CharField(
         max_length=255,
         help_text=_(
             'If the transaction’s net funds are available in the Stripe '
-            'balance yet. Either ``available`` or ``pending``.'
-        )
+            'balance yet. Either ``available`` or ``pending``.',
+        ),
     )
     type = models.CharField(
         max_length=255,
         help_text=_(
             'Type of the transaction, one of: ``charge``, ``refund``, '
             '``adjustment``, ``application_fee``, ``application_fee_refund``, '
-            '``transfer``, ``transfer_cancel`` or ``transfer_failure``.'
-        )
+            '``transfer``, ``transfer_cancel`` or ``transfer_failure``.',
+        ),
     )
     description = models.CharField(max_length=255)
     source = json.JSONField(
         help_text=_(
-            'The Stripe object this transaction is related to.'
-        )
+            'The Stripe object this transaction is related to.',
+        ),
     )
     sourced_transfers = json.JSONField(
         help_text=_(
-            'The transfers (if any) for which source is a source_transaction.'
-        )
+            'The transfers (if any) for which source is a source_transaction.',
+        ),
     )
