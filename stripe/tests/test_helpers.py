723976c0f215a36d528c7ca7707b7fc153f217de
Tony Narlock <tony@git-pull.com>
2017-12-14 07:49:32 -0600

    update tests to use pytest

--- stripe/tests/test_helpers.py
+++ stripe/tests/test_helpers.py
@@ -4,8 +4,6 @@ from __future__ import unicode_literals
 
 import json
 
-from django.test import TestCase
-
 import responses
 import stripe
 
@@ -14,29 +12,26 @@ from .helpers import open_test_file
 stripe.api_key = 'sk_test_BQokikJOvBiI2HlWgH4olfQ2'
 
 
-class TestResponsesStripeSanity(TestCase):
+@responses.activate
+def test_my_api():
+    """Tests to assure the Stripe library is using the Requests library as an
+    HTTP Client and responses is mocking the responses as intended."""
 
-    """Tests to assure the Stripe library is using the Requests
-    library as an HTTP Client and responses is mocking the
-    responses as intended."""
+    body_json = open_test_file('customer/object.json').read()
 
-    @responses.activate
-    def test_my_api(self):
-        body_json = open_test_file('customer/object.json').read()
+    customer_id = json.loads(body_json)['id']
+    customer_url = 'https://api.stripe.com/v1/customers/%s' % customer_id
+    responses.add(
+        responses.GET,
+        customer_url,
+        body=body_json,
+        status=200,
+        content_type='application/json',
+    )
 
-        customer_id = json.loads(body_json)['id']
-        customer_url = 'https://api.stripe.com/v1/customers/%s' % customer_id
-        responses.add(
-            responses.GET,
-            customer_url,
-            body=body_json,
-            status=200,
-            content_type='application/json',
-        )
+    customer = stripe.Customer.retrieve(customer_id)
 
-        customer = stripe.Customer.retrieve(customer_id)
-
-        self.assertEquals(customer.id, customer_id)
-        self.assertEquals(len(responses.calls), 1)
-        self.assertEquals(responses.calls[0].request.url, customer_url)
-        self.assertEquals(responses.calls[0].response.text, body_json)
+    assert customer.id == customer_id
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == customer_url
+    assert responses.calls[0].response.text == body_json
