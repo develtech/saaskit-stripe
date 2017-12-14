2dfc756ef1aa4c4e8849f2f01d4b498122246d50
Tony Narlock <tony@git-pull.com>
2017-12-14 10:29:40 -0600

    show example case of create Customer from api response

--- tests/test_customer.py
+++ tests/test_customer.py
@@ -2,13 +2,26 @@
 
 from stripe.resource import Customer as StripeCustomer
 
+from ..models import Customer
 from ..test import get_test_data, mock_stripe_response
 
 
-def test_customer(stripe, monkeypatch):
+def test_customer_mock(stripe, monkeypatch):
     data = get_test_data('customer/object.json')
 
     monkeypatch.setattr(StripeCustomer, 'create', mock_stripe_response(data))
 
     customer = StripeCustomer.create()
     assert customer['id'] == data['id']
+
+
+def test_customer_model(stripe, monkeypatch):
+    data = get_test_data('customer/object.json')
+
+    monkeypatch.setattr(StripeCustomer, 'create', mock_stripe_response(data))
+
+    customer = StripeCustomer.create()
+    cdict = customer.to_dict()
+    cdict.pop('object')
+    cdict.pop('subscriptions')
+    c = Customer(**cdict)
