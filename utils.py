8e1443f5f0b579d82b78dec6dcc35d5ed4c9e54b
Tony Narlock <tony@git-pull.com>
2017-12-15 04:58:17 -0600

    add some comments

--- utils.py
+++ utils.py
@@ -7,6 +7,14 @@ import pytz
 
 
 def handle_unix_timefields(Model, _dict):
+    """Parse unix epoch time values into datetime.datetime
+
+    :param Model: model whose fields to iterate over
+    :type Model: :class:`django.db.models.Model`
+    :param _dict: dict of values
+    :type: :class:`dict`
+    :rtype: :class:`datetime.datetime`
+    """
     for field in Model._meta.get_fields():
         if isinstance(field, models.DateTimeField):
             _dict[field.name] = datetime.datetime.fromtimestamp(
