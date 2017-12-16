ce6fdb5cb789b0c1a1de3887a92833dc566aaea8
Tony Narlock <tony@git-pull.com>
2017-12-16 07:48:11 -0600

    add bank_account, generic PaymentMethod model, fixes for sources

---
+++ stripe/models/bank_account.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from django_extensions.db.fields import json
+
+from .charge import CURRENCY_CHOICES
+from .payment_method import PaymentMethod
+
+BANK_ACCOUNT_TYPES = (
+    ('individual', _('Individual')),
+    ('company', _('Company')),
+)
+
+BANK_ACCOUNT_STATUS = (
+    ('new', _('New')),
+    ('validated', _('Validated')),
+    ('verified', _('Verified')),
+    ('verification_failed', _('Verification Failed')),
+    ('errored', _('Errored')),
+)
+
+
+class BankAccount(PaymentMethod):
+    account = models.ForeignKey('account')
+    account_holder_name = models.CharField(max_length=255)
+    account_type = models.CharField(max_length=255, choices=BANK_ACCOUNT_TYPES)
+    bank_name = models.CharField(max_length=255)
+    currency = models.CharField(max_length=255, choices=CURRENCY_CHOICES)
+    customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
+    default_for_currency = models.BooleanField()
+    fingerprint = models.CharField(max_length=255)
+    last4 = models.CharField(max_length=4)
+    metadata = json.JSONField()
+    routing_number = models.CharField(max_length=255)
+    status = models.CharField(max_length=255, choices=BANK_ACCOUNT_STATUS)
---
+++ stripe/models/payment_method.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+As of December 16th, 2017, Stripe breaks payment methods into 3 categories:
+
+- Bank Accounts
+- Cards
+- Sources
+
+They must be retrieved through the Stripe API customer object.
+
+Of these, the strangest of them is source. They're not listable, and directly
+query-able. Some sources aren't reuseable:
+https://stripe.com/docs/sources#supported-payment-methods
+
+"""
+
+from django.db import models
+
+
+class PaymentMethod(models.Model):
+    id = models.AutoField(primary_key=True)
