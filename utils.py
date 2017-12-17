dcf3bb68ee933c850cc9d8a0a0ae9c69d8dd6b51
Tony Narlock <tony@git-pull.com>
2017-12-17 08:34:01 -0600

    Split off customer info insertion from from_stripe_object

--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import datetime
 
+from django.apps import apps
 from django.db import models
 
 import pytz
+import stripe
 
 
 class UnixDateTimeField(models.DateTimeField):
@@ -19,3 +21,26 @@ class UnixDateTimeField(models.DateTimeField):
                 )
                 setattr(model_instance, self.attname, new_value)
         return super().pre_save(model_instance, add)
+
+
+def get_customer_info(_dict, customer):
+    """Fill in customer info inn from_stripe_object
+
+    This is specialized for the "_dict" convention passed around in
+    from_stripe_object.
+    """
+    Customer = apps.get_model('stripe.Customer')
+    # Handle situations like Charge where customer is optional
+    customer_id = None
+    if 'customer' in _dict:
+        customer_id = _dict.pop('customer')
+
+    if customer:
+        _dict['customer'] = customer
+    elif customer_id:
+        customer_object = stripe.Customer.retrieve(customer_id)
+        _dict['customer'] = Customer.from_stripe_object(
+            customer_object,
+            descend=False,
+        )
+    return _dict
