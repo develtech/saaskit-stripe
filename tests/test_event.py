25ced4869514824842a043b3b116dc9d98c898a7
Tony Narlock <tony@git-pull.com>
2017-12-18 14:45:03 -0600

    adjust testname for event test

--- tests/test_event.py
+++ tests/test_event.py
@@ -7,7 +7,7 @@ from ..test import skip_if_stripe_mock_server_offline
 
 @skip_if_stripe_mock_server_offline
 @pytest.mark.django_db(transaction=True)
-def test_plan_model_mock(mock_stripe):
+def test_event_model_mock(mock_stripe):
     event_list = mock_stripe.Event.list()
     assert len(event_list.data)
     for stripe_object in event_list.auto_paging_iter():
